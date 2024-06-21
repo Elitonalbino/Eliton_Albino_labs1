d9a53884b011e5190175b7fff942c520c53be87f
Elitonalbino <41269885+Elitonalbino@users.noreply.github.com>
2024-06-21 18:56:21 -0300

    Update AnoBissexto.java

--- exercicios02/AnoBissexto.java
+++ exercicios02/AnoBissexto.java
@@ -7,7 +7,7 @@ public class AnoBissexto {
         System.out.println("Digite o ano: ");
         int ano = bissexto.nextInt(); 
         
-        if(ano % 400 == 0 || ano %4 == 0 && ano % 100 != 0 ){
+        if(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0 ){
           System.out.println( ano +" E ano Bissexto");
         }else{
             System.out.println(" Não e ano bissexto");
