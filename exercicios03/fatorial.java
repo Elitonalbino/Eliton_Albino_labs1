788d97f40269a939ebc68dac8c869f10285868ed
Elitonalbino <41269885+Elitonalbino@users.noreply.github.com>
2024-08-05 19:39:06 -0300

    Update fatorial.java

--- exercicios03/fatorial.java
+++ exercicios03/fatorial.java
@@ -21,3 +21,27 @@ public class fatorial {
     }
 
 
+package exercicios03;
+
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int f = 0;
+        System.out.println("digite o fatorial: ");
+        f = scan.nextInt();
+        
+        int fato = 1;
+          for(int i=f; i>0; i--){
+            fato *= i;
+            System.out.println(i);
+            }
+        System.out.println("resultado: " + fato);
+    
+        scan.close();
+        }
+    }
+
+
+>>>>>>> Stashed changes
