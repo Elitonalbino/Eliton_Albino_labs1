795d12752141815afb6707e1674365e394d59a53
Elitonalbino <41269885+Elitonalbino@users.noreply.github.com>
2024-08-05 19:41:38 -0300

    Update conjunton.java

--- exercicios03/conjunton.java
+++ exercicios03/conjunton.java
@@ -44,4 +44,51 @@ public class conjunton {
         scan.close();   
     }
     
+package exercicios03;
+
+import java.util.Scanner;
+
+public class conjunton {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Quantos números você vai inserir? ");
+        int n = scan.nextInt();
+        int[] numeros = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (true) {
+                System.out.print("Digite um número entre 0 e 1000: ");
+                int num = scan.nextInt();
+                if (num >= 0 && num <= 1000) {
+                    numeros[i] = num;
+                    break;
+                } else {
+                    System.out.println("Número inválido. Tente novamente.");
+                }
+            }
+        }
+
+        int menorValor = numeros[0];
+        int maiorValor = numeros[0];
+        int somaValores = 0;
+
+        for (int num : numeros) {
+            if (num < menorValor) {
+                menorValor = num;
+            }
+            if (num > maiorValor) {
+                maiorValor = num;
+            }
+            somaValores += num;
+        }
+
+        System.out.println("Menor valor: " + menorValor);
+        System.out.println("Maior valor: " + maiorValor);
+        System.out.println("Soma dos valores: " + somaValores);
+
+        scan.close();   
+    }
+    
+>>>>>>> Stashed changes
 }
